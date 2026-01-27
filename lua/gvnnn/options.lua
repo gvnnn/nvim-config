5f90e58574ae450ebc3e17f9bb3cebdda5f2b4ce
gvnnn <17831785+gvnnn@users.noreply.github.com>
2026-01-27 22:26:17 +0100

    Minor changes

--- lua/gvnnn/options.lua
+++ lua/gvnnn/options.lua
@@ -1,6 +1,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- lua/gvnnn/plugins/catppuccin.lua
+++ lua/gvnnn/plugins/catppuccin.lua
@@ -5,9 +5,17 @@ return {
 	opts = {
 		styles = {
 			comments = {'italic'}
-		}
+		},
+		custom_highlights = function(colors)
+			return {
+				LineNr = { fg = colors.subtext1 },
+				NonText = { fg = colors.overlay0 },
+				Whitespace = { fg = colors.overlay0 },
+			}
+		end,
 	},
-	config = function()
+	config = function(_, opts)
+		require('catppuccin').setup(opts)
 		vim.cmd([[colorscheme catppuccin]])
 	end
 }
