e3f8b4f08c72958da3c6c0c0bb73e168a97c0699
Gianluca Stella <gistella@redhat.com>
2026-03-04 08:36:20 +0100

    Minor updates

--- lua/gvnnn/mappings.lua
+++ lua/gvnnn/mappings.lua
@@ -7,6 +7,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fO', builtin.lsp_document_symbols, { desc = 'Telescope symbols' })
 
 vim.keymap.set({'n', 'v'}, 'gy', '"+y', { desc = 'copy to system clipboard' })
 vim.keymap.set({'n', 'v'}, 'gp', '"+p', { desc = 'paste from system clipboard' })
