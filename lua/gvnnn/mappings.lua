67083df5e74eb2a6be145ae6358e951abb8eb346
Gianluca Stella <gistella@redhat.com>
2026-03-05 10:35:52 +0100

    Add gitsigns, mappings

--- lua/gvnnn/mappings.lua
+++ lua/gvnnn/mappings.lua
@@ -21,3 +21,5 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", '<C-w>e', '<C-w>v<C-]>')
