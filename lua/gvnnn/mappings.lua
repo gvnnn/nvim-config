502d6fe1a0cd66c783d73135782614b83fbb22f8
gvnnn <17831785+gvnnn@users.noreply.github.com>
2026-03-06 19:58:17 +0100

    Add some mappings, use tagged catppuccin versions

--- lua/gvnnn/mappings.lua
+++ lua/gvnnn/mappings.lua
@@ -22,4 +22,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- jump to definition in a vertically-split window
 vim.keymap.set("n", '<C-w>e', '<C-w>v<C-]>')
+
+-- paste without overwriting the unnamed register
+vim.keymap.set("x", "<leader>p", [["_dP]])
